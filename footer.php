a10d3af5b9775dd23ad85b8e30ade8e5a07e3c6f
Alex McIver <alexmciver@ymail.com>
2026-09-09 19:04:33 +0100

    Implement footer layout with responsive design and add ACF JSON configuration for footer block

--- footer.php
+++ footer.php
@@ -4,7 +4,30 @@
 
 <div class="footer">
  <div class="container">
-  <div class="content"></div>
+  <div class="footer-content">
+  <div class="col-left">
+   <h2>Alex M Web Design</h2>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+  </div>
+  <div class="col-middle">
+   <h2>Site</h2>
+   <ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Services</a></li>
+    <li><a href="#">Contact</a></li>
+   </ul>
+  </div>
+  <div class="col-right">
+   <h2>Contact</h2>
+   <p>Email: info@alexmwebdesign.com</p>
+   <p>Phone: 123-456-7890</p>
+   <p>Address: 123 Main St, Anytown, USA</p>
+ </div>
+</div>
+ <hr>
+ <div class="copyright">
+  <h3>Copyright &copy; <?php echo date('Y'); ?> Alex M Web Design. All rights reserved.</h3>
  </div>
 </div>
 
