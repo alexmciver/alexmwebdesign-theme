65439cfe3424375ab905da93b60e7db1e78219e6
Alex McIver <alexmciver@ymail.com>
2026-09-14 11:31:31 +0100

    Refactor Google Tag Manager integration in header; streamline script and noscript sections for improved clarity and compliance with user consent requirements.

--- header.php
+++ header.php
@@ -8,19 +8,20 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<?php // GTM blocked until Complianz Statistics consent (Complianz must not also inject GTM). ?>
-	<script type="text/plain" data-category="statistics">
-	(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','GTM-NZVWTWP');
-	</script>
+	})(window,document,'script','dataLayer','GTM-NZVWTWP');</script>
+	<!-- End Google Tag Manager -->
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+<!-- Google Tag Manager (noscript) -->
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NZVWTWP"
-height="0" width="0" style="display:none;visibility:hidden" data-category="statistics"></iframe></noscript>
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <?php wp_body_open(); ?>
 
 <a class="skip-link" href="#content"><?php esc_html_e( 'Skip to content', 'alex-theme' ); ?></a>
